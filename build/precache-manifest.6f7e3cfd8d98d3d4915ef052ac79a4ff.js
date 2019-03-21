0a2d673830388fb7e81b0f548282bb5cf969eb94
OshadhaSenanayake <oshadhakawshalya@gmail.com>
2019-03-21 19:40:02 +0530

    clean up the code

---
+++ build/precache-manifest.6f7e3cfd8d98d3d4915ef052ac79a4ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f0cdd5da58382aaf4ee5",
+    "url": "/static/js/main.5a321d1a.chunk.js"
+  },
+  {
+    "revision": "3e0dc1bb8f2c4bba8dbf",
+    "url": "/static/js/2.85996f6a.chunk.js"
+  },
+  {
+    "revision": "f0cdd5da58382aaf4ee5",
+    "url": "/static/css/main.572c6fb3.chunk.css"
+  },
+  {
+    "revision": "3e0dc1bb8f2c4bba8dbf",
+    "url": "/static/css/2.c7976949.chunk.css"
+  },
+  {
+    "revision": "05b54dc3b54911b2712cecd67c1887ae",
+    "url": "/index.html"
+  }
+];
